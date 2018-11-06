bafb067cdb1d3fe8a55302d51636105a4cd54e00
yiguid <11309194@qq.com>
2018-11-05 21:29:41 -0600

    two point with record, slide window

--- 003.py
+++ 003.py
@@ -14,6 +14,20 @@ class Solution:
 
         return l
 
+    def lengthOfLongestSubstring2(self,s):
+        f = [0] * 256
+        l = 0
+        r = -1
+        res = 0
+        while l < len(s):
+            if r + 1 < len(s) and f[ord(s[r + 1])] == 0:
+                r += 1
+                f[ord(s[r])] += 1
+            else:
+                f[ord(s[l])] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
 
 s = Solution()
 print(s.lengthOfLongestSubstring(' '))
@@ -21,3 +35,4 @@ print(s.lengthOfLongestSubstring('abba'))
 print(s.lengthOfLongestSubstring('pwwkew'))
 print(s.lengthOfLongestSubstring('bbbb'))
 print(s.lengthOfLongestSubstring('abcabcbb'))
+print(s.lengthOfLongestSubstring2('pwwkek'))
